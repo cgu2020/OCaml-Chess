0c39dcfde43ce9554eac1812592eea47f438fc1d
Justin Gu <jjg282@cornell.edu>
2021-03-31 18:05:52 -0400

    update version to be worked on 3/31

--- _build/game_logic.ml
+++ _build/game_logic.ml
@@ -11,14 +11,12 @@ val move_bishop : tile -> tile list
 
 val move_king : tile -> tile list*)
 
-exception NonValidFirstPiece
+(**open Tile
 
-exception NonValidMove
-let check_validity (tile1: Tile.tile) tile2 = match tile1 with
-  | Empty -> raise NonValidFirstPiece
-  | _  ->
-    if tile2 = Empty then true (*Change empty to tile 2 in in the list of move_piecename *)
-    else if tile2.team = tile1.team then 
-      raise NonValidMove
-    else false
+exception InvalidMove
+let check_validity (tile1 : Tile.tile) (tile2: Tile.tile) =
+  let x = Tile.get_piece tile1 in 
+  match x with 
+  |Pawn -> true
+  |_ -> false*)
 
--- _build/tile.mli
+++ _build/tile.mli
@@ -1,3 +1,11 @@
-type tile
+type tile 
+
+type p
 
 val point_value : tile -> int
+
+val get_piece : tile -> p
+
+val empty_tile : tile
+
+val pawn : tile
