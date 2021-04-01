314bb57d8f348a3ef765fd0ea71d3567df38ba1a
Chris Gu <cjg245@cornell.edu>
2021-04-01 02:44:41 -0400

    Working textboard

--- _build/main.ml
+++ _build/main.ml
@@ -1,2 +1,31 @@
-let main () =
-  failwith "unimplemented"
+open Chess_board
+open Tile
+
+let string_piece tile =
+  match get_piece tile with
+  |Pawn -> "p"
+  |Rook -> "r"
+  |Knight -> "n"
+  |Bishop -> "b"
+  |King -> "k"
+  |Queen -> "q"
+  |Empty -> ","
+
+
+let rec print_row x y b: unit = match y with 
+| 8 -> print_string "\n"
+| y -> print_string (string_piece b.(x).(y) ^ "  "); print_row x (y+1) b
+
+let rec print_board x b: unit =
+  match x with
+  | 8 -> ()
+  | x -> print_row x 0 b; print_board (x+1) b
+
+let print b= print_board 0 b
+
+let main () = 
+  let b = init in
+  initialize b (string_to_lists starterboard) 0;
+  print b
+
+let () = main()
--- chess_board.mli
+++ chess_board.mli
@@ -1,14 +1,21 @@
 
 (** The abstract type that represents a chessboard. *)
-type board
+type board = Tile.tile array array
 
 exception InvalidMove
 
 (** [possible_moves a] is a set-like list of all the tiles 
     that a can move to.*)
+
 val possible_moves : board -> board list
 
-val replace_tile : int -> int -> int -> int -> unit (**Helper for move piece*)
+val string_to_lists : string -> string list list
+
+val starterboard : string
+
+val initialize : board -> string list list -> int -> unit
+
+val init : board
 
 val clear_tile : board -> unit (**Helper for move_piece*)
 
