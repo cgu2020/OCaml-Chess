8c472762c8e8f827d0dad6b0de21415bc0bd59fc
Justin Gu <jjg282@cornell.edu>
2021-04-25 21:20:18 -0400

    Working main with switching player moves

--- chess_board.mli
+++ chess_board.mli
@@ -1,6 +1,5 @@
 (** The abstract type that represents a chessboard. *)
 type board = Tile.tile array array
-
 exception InvalidMove
 
 (** [possible_moves a] is a set-like list of all the tiles 
@@ -11,11 +10,6 @@ val possible_moves : Tile.tile -> int*int -> (int * int) list
     to tile b is a valid move*)
 (**val check_validity : board -> int -> int -> int -> int -> bool*)
 
-
-val replace_tile : int -> int -> int -> int -> unit (**Helper for move piece*)
-
-val clear_tile : board -> int -> int -> unit (**Helper for move_piece*)
-
 val string_to_lists : string -> string list list
 
 val starterboard : string
@@ -24,6 +18,11 @@ val initialize : board -> string list list -> int -> unit
 
 val init : board
 
-(** [move_piece a b] clears tile a and replaces tile b with tile a
+
+val check_validity :  board -> int -> int -> int-> int -> int -> bool
+
+(** [move_piece b x1 y1 x2 y2 c] clears tile (x1,y1) and replaces tile (x2,y2) with tile (x1, y1). 
+Invalid move if the int c does not match the color
 Raises [Invalid t] if [t] is not a valid tile to move to.  *)
-val move_piece : board -> int -> int -> int-> int -> unit
+val move_piece : board -> int -> int -> int-> int -> int -> unit
+
--- command.mli
+++ command.mli
@@ -2,6 +2,7 @@ type object_phrase = string list
 
 type command =
   | Move of object_phrase
+  | InvalidMove
   | Quit
 
 exception Empty
